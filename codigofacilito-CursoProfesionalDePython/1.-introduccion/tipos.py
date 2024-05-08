27380412d03a16d1ce42992d35c3dd01d29e0e42
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-05-08 16:23:02 -0500

    Modulo 01: tipos de datos

---
+++ codigofacilito-CursoProfesionalDePython/1.-introduccion/tipos.py
@@ -0,0 +1,28 @@
+# String
+titulo_curso = 'Curso profesional de Python 🐍'
+print(titulo_curso)
+
+nombre_completo = "Grimaldo 🧑‍🦳"
+print(nombre_completo)
+
+mensaje = "'CódigoFacilito'" #'"CódigoFacilito"'
+print(mensaje)
+
+mensaje = '''Te encuentras 
+en el curso: profesional de Python.
+En CódigoFacilito'''
+print(mensaje)
+
+# Int
+# Al dividir con // obtenemos un tipo de dato entero
+# Al dividir con / Obtenemos un tipo de dato flotanto
+numero_uno = 10 // 3
+print(numero_uno)
+
+# Float
+numero_dos = -3.14
+print(numero_dos)
+
+# Bool
+valor = True
+print(valor)
